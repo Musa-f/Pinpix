3021504501ffe5c9659bc7627f27891e4012f702
musa-f <musaevafatima77@gmail.com>
2023-02-02 16:20:10 +0100

    change

---
+++ Site/vue/footer.php
@@ -0,0 +1,36 @@
+<footer>
+        <h1>Infos utile</h1>
+        <div class="services">
+            <div>
+                <nav class="nav-footer">
+                    <ul>
+                        <li><a href="">Qui Sommes-Nous?</a></li>
+                        <li><a href="">Communauté</a></li>
+                        <li><a href="">Rejoindre l'équipe</a></li>
+                        <li><a href="">Centre d'assistance</a></li>
+                        <li><a href="">Charte de protection des données</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <nav class="nav-footer">
+                    <ul>
+                        <li><a href="">Qui Sommes-Nous?</a></li>
+                        <li><a href="">Communauté</a></li>
+                        <li><a href="">Rejoindre l'équipe</a></li>
+                        <li><a href="">Centre d'assistance</a></li>
+                        <li><a href="">Charte de protection des données</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+        <p id="end">&copy; 2023, PinPix.</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/fonction.js"></script>
+</body>
+
+</html>
+</body>
