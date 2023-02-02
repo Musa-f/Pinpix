176754003438fef3f52677f03f1cf64f65c0d7b2
Nicos65 <dubroca.nicolas0543@gmail.com>
2023-02-02 15:26:02 +0100

    change index

--- Site/index.php
+++ Site/index.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PinPix</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../assets/css/styleall.css">
+    <link rel="stylesheet" href="assets/css/styleall.css">
 </head>
 
 <body>
     <header>
         <nav>
             <div class="div_nav">
-                <a href="#"><img class="logo" src="../assets/ressources/icons/logo.png"></a>
+                <a href="#"><img class="logo" src="assets/ressources/icons/logo.png"></a>
                 <a href="#">
                     <h1>PinPix</h1>
                 </a>
