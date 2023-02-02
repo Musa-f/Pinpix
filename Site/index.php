5b51fec27b91bc78409376397eaa57f3a7c51589
Nicos65 <dubroca.nicolas0543@gmail.com>
2023-02-02 15:56:12 +0100

    push push plz!!!!

--- Site/index.php
+++ Site/index.php
@@ -17,12 +17,50 @@
                     <h1>PinPix</h1>
                 </a>
             </div>
+            <input placeholder="rechercher">
             <div class="onglets">
                 <a href="index.php">Accueil</a>
                 <a href="header/contact.php">Contact</a>
                 <a href="header/compte.php">Compte</a>
-
             </div>
         </nav>
     </header>
+    <section class="section_index">
+        <h2>"Des souvenirs éternels avec PinPix"</h2>
+        <p>Rejoignez une communauté passionnée de photographie en ligne ! Avec ce site, vous pouvez partager vos plus belles photos avec le monde entier et découvrir les talents de photographes du monde entier. Suivez les galeries de vos photographes préférés, likez les photos qui vous inspirent. Créez votre propre galerie pour montrer votre créativité et votre style unique.
+            Inscrivez-vous dès maintenant et faites partie d'une communauté en constante évolution de passionnés de la photographie.</p>
+    </section>
+    <footer>
+        <h1>Infos utile</h1>
+        <div class="services">
+            <div>
+                <nav class="nav-footer">
+                    <ul>
+                        <li><a href="">Qui Sommes-Nous?</a></li>
+                        <li><a href="">Communauté</a></li>
+                        <li><a href="">Rejoindre l'équipe</a></li>
+                        <li><a href="">Centre d'assistance</a></li>
+                        <li><a href="">Charte de protection des données</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <nav class="nav-footer">
+                    <ul>
+                        <li><a href="">Qui Sommes-Nous?</a></li>
+                        <li><a href="">Communauté</a></li>
+                        <li><a href="">Rejoindre l'équipe</a></li>
+                        <li><a href="">Centre d'assistance</a></li>
+                        <li><a href="">Charte de protection des données</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+        <p id="end">&copy; 2023, PinPix.</p>
+    </footer>
+    <script src="assets/js/fonction.js"></script>
+</body>
+
+</html>
 </body>
