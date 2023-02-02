f03a0487e25a905bd9fe082c4d261d1d8bdb824c
Nicos65 <dubroca.nicolas0543@gmail.com>
2023-02-02 15:22:26 +0100

    debut test

---
+++ Site/vue/index.php
@@ -0,0 +1,28 @@
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PinPix</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../assets/css/styleall.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="div_nav">
+                <a href="#"><img class="logo" src="../assets/ressources/icons/logo.png"></a>
+                <a href="#">
+                    <h1>PinPix</h1>
+                </a>
+            </div>
+            <div class="onglets">
+                <a href="index.php">Accueil</a>
+                <a href="header/contact.php">Contact</a>
+                <a href="header/compte.php">Compte</a>
+
+            </div>
+        </nav>
+    </header>
+</body>
