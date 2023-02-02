95929e87dd6bb333ed2000d30bd78015a2f4a46c
musa-f <musaevafatima77@gmail.com>
2023-02-02 16:25:58 +0100

    push

--- Site/vue/header.php
+++ Site/vue/header.php
@@ -7,21 +7,21 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="assets/css/styleall.css">
+    <link rel="stylesheet" href="../assets/css/styleall.css">
 </head>
 
 <body>
     <header>
         <nav>
             <div class="div_nav">
-                <a href="#"><img class="logo" src="assets/ressources/icons/logo.png"></a>
+                <a href="#"><img class="logo" src="../assets/ressources/icons/logo.png"></a>
                 <a href="#">
                     <h1>PinPix</h1>
                 </a>
             </div>
             <form>
             <input type="text" name="recherche" id="recherche" placeholder="rechercher">
-            <button type="submit"> <img src=""></button>
+            <button type="submit"><i class="bi bi-search"></i></button>
 
 </form>
             <div class="onglets">
