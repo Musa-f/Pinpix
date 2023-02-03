291cd09e9db32d27ffde5425aa9d9287879e0149
musa-f <musaevafatima77@gmail.com>
2023-02-03 10:36:31 +0100

    changes css

--- Site/view/header-connexion.php
+++ Site/view/header-connexion.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PinPix</title>
     <meta charset="utf-8">
+    <link rel="icon" type="image/x-icon" href="/pinpix/site/assets/ressources/icons/favicon.ico">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../assets/css/styleall.css">
