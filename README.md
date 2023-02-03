240d3721784ffe08250aaa46ce5f71041ca89b2c
musa-f <musaevafatima77@gmail.com>
2023-02-03 12:36:42 +0100

    changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pinpix
+
+Dimensions images:
+-Miniatures photos: 200*200
+-Avatar : ??
--- Site/view/profil.php
+++ Site/view/profil.php
@@ -44,53 +44,46 @@
                 <div class="gallery-images">
                     
                     <div class="box d-flex flex-column">
-                        <div class="d-flex justify-content-between">
-                            <p>@username <i class="bi bi-heart-fill"></i></p>
-                            <p>189<i class="bi bi-hand-thumbs-up"></i></p>
+                        <div class="d-flex justify-content-end">
+                            <p>189<i class="bi bi-hand-thumbs-up-fill"></i></p>
                         </div>
                         <img src="https://fastly.picsum.photos/id/641/200/200.jpg?hmac=9pd71nRRRsT7TXf0zn0hQ6tW6VQnQ-UtL1JXDhJZB8E" alt="">
                     </div>
 
                     <!-- Divs à supprimer plus tard -->
                     <div class="box d-flex flex-column">
-                        <div class="d-flex justify-content-between">
-                            <p>@username <i class="bi bi-heart-fill"></i></p>
+                        <div class="d-flex justify-content-end">
                             <p>189<i class="bi bi-hand-thumbs-up"></i></p>
                         </div>
                         <img src="https://fastly.picsum.photos/id/605/200/200.jpg?hmac=aAxC3x4fmu7RXhLH-1idijrAUvlZVEwlUqjzKR80YWo" alt="">
                     </div>
                     <div class="box d-flex flex-column">
-                        <div class="d-flex justify-content-between">
-                            <p>@username <i class="bi bi-heart-fill"></i></p>
+                        <div class="d-flex justify-content-end">
                             <p>189<i class="bi bi-hand-thumbs-up"></i></p>
                         </div>
                         <img src="https://fastly.picsum.photos/id/790/200/200.jpg?hmac=Y1d81XFNx8LJhlNsiwDoDgIn4mF3SK9nTdIVqkkHS9I" alt="">
                     </div>
                     <div class="box d-flex flex-column">
-                        <div class="d-flex justify-content-between">
-                            <p>@username <i class="bi bi-heart-fill"></i></p>
-                            <p>189<i class="bi bi-hand-thumbs-up"></i></p>
+                        <div class="d-flex justify-content-end">
+                            <p>189<i class="bi bi-hand-thumbs-up-fill"></i></p>
                         </div>
                         <img src="https://fastly.picsum.photos/id/334/200/200.jpg?hmac=Q9rDA3ngheQsAB7HoLSjpzYS0kqelfZIJBGDkW-4wgk" alt="">
                     </div>
                     <div class="box d-flex flex-column">
-                        <div class="d-flex justify-content-between">
-                            <p>@username <i class="bi bi-heart-fill"></i></p>
+                        <div class="d-flex justify-content-end">
                             <p>189<i class="bi bi-hand-thumbs-up"></i></p>
                         </div>
                         <img src="https://fastly.picsum.photos/id/141/200/200.jpg?hmac=cy5VCF6f8Mq7cum6d-R7m7mbrGC0wgUzI4UpSX_qkIk" alt="">
                     </div>
                     <div class="box d-flex flex-column">
-                        <div class="d-flex justify-content-between">
-                            <p>@username <i class="bi bi-heart-fill"></i></p>
+                        <div class="d-flex justify-content-end">
                             <p>189<i class="bi bi-hand-thumbs-up"></i></p>
                         </div>
                         <img src="https://fastly.picsum.photos/id/254/200/200.jpg?hmac=wM9u9N0tgdWKFIr8MxBLr8rLoV0JjUUKLk32XFV8agQ" alt="">
                     </div>
                     <div class="box d-flex flex-column">
-                        <div class="d-flex justify-content-between">
-                            <p>@username <i class="bi bi-heart-fill"></i></p>
-                            <p>189<i class="bi bi-hand-thumbs-up"></i></p>
+                        <div class="d-flex justify-content-end">
+                            <p>189<i class="bi bi-hand-thumbs-up-fill"></i></p>
                         </div>
                         <img src="https://fastly.picsum.photos/id/738/200/200.jpg?hmac=s1pXxIw4TdDLyUY2RRVeDyhLPUF2xW3AFjdzjVsE6zc" alt="">
                     </div>
@@ -109,15 +102,40 @@
                     <div class="modal-dialog modal-dialog-centered" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLongTitle">Ajouter une image</h5>
+                                <h5 class="modal-title text-uppercase" id="exampleModalLongTitle">
+                                    <i class="bi bi-image-fill"></i>
+                                    Ajouter une image
+                                </h5>
                             </div>
-                            <div class="modal-body">
-                                ...
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                            </div>
+                            <form action="" method="post">
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <label for="name_image">Nom</label>
+                                        <input type="text" class="form-control" name="image_name">
+                                    </div>
+                                    <div class="form-group mt-2">
+                                        <label for="tags_image">Tags</label>
+                                        <select class="form-select" id="tags_image" name="tags_image">
+                                            <option>Nature</option>
+                                            <option>Animaux</option>
+                                            <option>Technologie</option>
+                                            <option>Design</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group mt-2">
+                                        <label for="upload_image" class="custom-file-label">Fichier</label>
+                                        <input type="file" class="form-control form-control-file" name="upload_image">
+                                    </div>
+                                    <div class="form-group mt-2">
+                                        <label for="description_image">Description</label>
+                                        <textarea name="description_image" cols="30" rows="6" class="form-control"></textarea>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                                    <button type="submit" name="submit" class="btn" id="submit_image">Enregistrer</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
