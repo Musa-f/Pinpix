4c28ad56fe144fb3568006ff796d694a2e7e9600
ValerieDoleac <114406228+ValerieDoleac@users.noreply.github.com>
2023-02-06 16:59:13 +0100

    updating css

---
+++ Site/view/contact.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="all.css">
+
+    <title>Document</title>
+</head>
+<body>
+<?php include('header.php');?>
+
+
+<div class="rectangle">Rectangle</div>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+            <h2 class="Title-pages">CONTACT</h2>
+                <div class="box rectangle">Rectangle</div>
+                <div id="moncercle"></div>
+
+
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<?php include('footer.php');?>
+</body>
+</html>
