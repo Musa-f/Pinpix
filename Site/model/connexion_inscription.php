3fca13adabc32a3d78ffb9ec2fa66c7cfcdcf580
Micael <zacio007@gmail.com>
2023-02-07 11:16:41 +0100

    Back coriger

--- Site/model/connexion_inscription.php
+++ Site/model/connexion_inscription.php
@@ -1,6 +1,7 @@
 <?php
 function verifConnexion($bdd, $user){
-    $data = selectUser($bdd, $user["id_user"]);
+    $data = selectAllUserById($bdd, $user["id_user"]);
+    $data = $data ->fetch();
     $_SESSION["id"] = $data["id_user"];
     $_SESSION["nom"] = $data["name_user"];
     $_SESSION["role"] = $data["id_role"];
