02fa48ee8c523dc39c2bc033e1cc23d2c359285d
ValerieDoleac <114406228+ValerieDoleac@users.noreply.github.com>
2023-02-07 09:05:53 +0100

    debug

--- Site/view/contact.php
+++ Site/view/contact.php
@@ -21,11 +21,34 @@
             <h2 class="Title-pages">CONTACT</h2>
                 <div class="box rectangle">Rectangle</div>
                 <div id="moncercle"></div>
+                <div id="rectangle"></div>
 
 
         </div>
     </div>
 </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+
+    <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">EMAIL</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">MOT DE PASSE</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
+            </div>
+        <div class="form-floating">
+            <textarea class="form-control" placeholder="Vôtre message" id="floatingTextarea2" style="height: 100px"></textarea>
+            <label for="floatingTextarea2">Commentaire</label>
+        </div>
+        <div class="form-group mt-4 mb-4 justify-content-center"></div>
+            <button type="submit" class="btn ">ENVOYER</button>
+        </div>
+    </form>
 
 
 
