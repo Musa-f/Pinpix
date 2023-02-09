ea53d6b1cf5823e340fea5c8a44b9cbc512bf8bb
Nicos65 <dubroca.nicolas0543@gmail.com>
2023-02-09 11:03:47 +0100

    changements accueil

--- Site/index.php
+++ Site/index.php
@@ -1,22 +1,10 @@
-<html lang="fr">
+<?php include('header.php'); ?>
+<section class="section_index">
+    <h2>"Des souvenirs éternels avec PinPix"</h2>
+    <p>Rejoignez une communauté passionnée de photographie en ligne ! Avec ce site, vous pouvez partager vos plus belles photos avec le monde entier et découvrir les talents de photographes du monde entier. Suivez les galeries de vos photographes préférés, likez les photos qui vous inspirent. Créez votre propre galerie pour montrer votre créativité et votre style unique.
+        Inscrivez-vous dès maintenant et faites partie d'une communauté en constante évolution de passionnés de la photographie.</p>
+</section>
+<section>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PinPix</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="assets/css/styleall.css">
-</head>
-
-<body>
-    <?php include('view/header.php'); ?>
-    <section class="section_index">
-        <h2>"Des souvenirs éternels avec PinPix"</h2>
-        <p>Rejoignez une communauté passionnée de photographie en ligne ! Avec ce site, vous pouvez partager vos plus belles photos avec le monde entier et découvrir les talents de photographes du monde entier. Suivez les galeries de vos photographes préférés, likez les photos qui vous inspirent. Créez votre propre galerie pour montrer votre créativité et votre style unique.
-            Inscrivez-vous dès maintenant et faites partie d'une communauté en constante évolution de passionnés de la photographie.</p>
-    </section>
-    <?php include('view/footer.php'); ?>
-</body>
-
-</html>
-</body>
+</section>
+<?php include('footer.php'); ?>
---
+++ Site/view/homepage.php
@@ -0,0 +1,10 @@
+<?php include('header.php'); ?>
+<section class="section_index">
+    <h2>"Des souvenirs éternels avec PinPix"</h2>
+    <p>Rejoignez une communauté passionnée de photographie en ligne ! Avec ce site, vous pouvez partager vos plus belles photos avec le monde entier et découvrir les talents de photographes du monde entier. Suivez les galeries de vos photographes préférés, likez les photos qui vous inspirent. Créez votre propre galerie pour montrer votre créativité et votre style unique.
+        Inscrivez-vous dès maintenant et faites partie d'une communauté en constante évolution de passionnés de la photographie.</p>
+</section>
+<section>
+
+</section>
+<?php include('footer.php'); ?>
