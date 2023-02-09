dca3f8660f14149c75ab51283270d1f0b7b7827e
Nicos65 <dubroca.nicolas0543@gmail.com>
2023-02-09 10:26:52 +0100

    ajout like follow

--- Site/model/insert.php
+++ Site/model/insert.php
@@ -82,4 +82,40 @@ function insertUserGal($bdd,$id_gallery, $id_user){
        die("error : ".$e->getMessage());
    }
 }
+
+function insertLike($bdd,$id_image,$id_user){
+    try{
+         //On lie l' image par son ID à L'id de l'user
+        $req = $bdd->prepare(
+            "INSERT INTO likes(id_image, id_user) VALUES
+            (:id_image, :id_user)"
+        );
+        
+        $req->execute(array(
+            "id_image" => $id_image,
+            "id_user" => $id_user
+        ));
+        $req->closeCursor();
+    }catch(Exception $e){
+        die("error : ".$e->getMessage());
+    }
+}
+
+function insertFollow($bdd,$id_user_1,$id_user_2){
+    try{
+         //On lie l'id de l'utilisateur 1 a l'id de l'utilisateur 2
+        $req = $bdd->prepare(
+            "INSERT INTO follow(id_user_1, id_user_2) VALUES
+            (:id_user_1, :id_user_2)"
+        );
+        
+        $req->execute(array(
+            "id_user_1" => $id_user_1,
+            "id_user_2" => $id_user_2
+        ));
+        $req->closeCursor();
+    }catch(Exception $e){
+        die("error : ".$e->getMessage());
+    }
+}
 ?>
