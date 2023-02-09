29412f2ceae92d50f970db2b85398700e5048f82
musa-f <musaevafatima77@gmail.com>
2023-02-09 15:24:28 +0100

    modifs

--- Site/controller/controller.php
+++ Site/controller/controller.php
@@ -171,6 +171,7 @@ if(isset($_GET["page"])){
     $page.=".php";
     include("../view/$page"); 
 }    
+
     
 include("../view/footer.php");
 ?>
--- Site/view/footer.php
+++ Site/view/footer.php
@@ -26,6 +26,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="fonctions.js"></script>
     <script src="<?php echo $script ?>"></script>
     </body>
 
