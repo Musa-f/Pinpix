70b6dc1abc4ba5d21cd5374f7a9e648bef01ead7
musa-f <musaevafatima77@gmail.com>
2023-02-09 16:07:14 +0100

    changes

--- Site/assets/js/fonctions.js
+++ Site/assets/js/fonctions.js
@@ -1,7 +1,12 @@
+let connectedUser = document.getElementById('compte').textContent;
+console.log(connectedUser);
+
+
 function test(){
     $ajax
     url: "traitement.php"
     data: {
+        connectedUser:username,
         username:username
     }
 }
--- Site/view/footer.php
+++ Site/view/footer.php
@@ -1,34 +1,34 @@
-    <footer>
-        <h1>Infos utiles</h1>
-        <div class="services">
-            <div>
-                <nav class="nav-footer">
-                    <ul>
-                        <li><a href="">Qui Sommes-Nous?</a></li>
-                        <li><a href="">Communauté</a></li>
-                        <li><a href="">Rejoindre l'équipe</a></li>
-                        <li><a href="">Centre d'assistance</a></li>
-                        <li><a href="">Charte de protection des données</a></li>
+        <footer>
+            <h1>Infos utiles</h1>
+            <div class="services">
+                <div>
+                    <nav class="nav-footer">
+                        <ul>
+                            <li><a href="">Qui Sommes-Nous?</a></li>
+                            <li><a href="">Communauté</a></li>
+                            <li><a href="">Rejoindre l'équipe</a></li>
+                            <li><a href="">Centre d'assistance</a></li>
+                            <li><a href="">Charte de protection des données</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div>
+                    <ul class="reseaux">
+                        <li><a href=""><i class="bi bi-facebook"></i></a></li>
+                        <li><a href=""><i class="bi bi-instagram"></i></a></li>
+                        <li><a href=""><i class="bi bi-twitter"></i></a></li>
                     </ul>
-                </nav>
+                </div>
             </div>
-            <div>
-                <ul class="reseaux">
-                    <li><a href=""><i class="bi bi-facebook"></i></a></li>
-                    <li><a href=""><i class="bi bi-instagram"></i></a></li>
-                    <li><a href=""><i class="bi bi-twitter"></i></a></li>
-                </ul>
-            </div>
+        </footer>
+        <div class="d-flex align-items-center justify-content-center p-0">
+            <p>&copy; 2023, PinPix.</p>
         </div>
-    </footer>
-    <div class="d-flex align-items-center justify-content-center p-0">
-        <p>&copy; 2023, PinPix.</p>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="fonctions.js"></script>
-    <script src="<?php echo $script ?>"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+        <script src="../assets/js/fonctions.js"></script>
+        <script src="<?php echo $script ?>"></script>
     </body>
 
-    </html>
-    </body>
+</html>
