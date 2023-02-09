9a0f942e83d12d6d574a23d22aafb263d079daf5
musa-f <musaevafatima77@gmail.com>
2023-02-09 16:59:11 +0100

    changes

---
+++ Site/controller/fonctions.php
@@ -0,0 +1,9 @@
+<?php
+    $user1 = $_POST['user1'];
+    $user2 = $_POST['user2'];
+    insertFollow($bdd, $user1, $user2);
+
+    $userLike = $_POST[''];
+    $imgLike = $_POST[''];
+    insertLike($bdd, $userLike, $imgLike)
+?>
