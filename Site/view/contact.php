3b715ec2c30c13b5cf15a9539dcfb10d5a6271ca
Nicos65 <dubroca.nicolas0543@gmail.com>
2023-02-14 09:02:02 +0100

    test

--- Site/view/contact.php
+++ Site/view/contact.php
@@ -1,22 +1,25 @@
-<div class="container">
+<div class="resp_contact container">
     <div class="row">
-        <div class="col-8">
-
+        <div class="col-1 col-lg-6 col-xl-12">
             <h2 class="titles-pages">CONTACT</h2>
             <div class="box">
                 <div id="moncercle"></div>
                 <div class="rectangle ">
                     <form class="form-control form-rectangle">
                         <div class="form-group mt-3 mb-2">
-                            <label for="exampleInputEmail1">EMAIL</label>
+                            <label for="exampleInputEmail1">Nom</label>
                             <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email">
                         </div>
-                        <div class="form-group mt-3 mb-3">
-                            <label for="exampleInputPassword1">MOT DE PASSE</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
+                        <div class="form-group mt-3 mb-2">
+                            <label for="exampleInputEmail1">Prenom</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email">
+                        </div>
+                        <div class="form-group mt-3 mb-2">
+                            <label for="exampleInputEmail1">Email</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email">
                         </div>
                         <div class="form-group mt-3 mb-4">
-                            <label for="floatingTextarea2">COMMENTAIRE</label>
+                            <label for="floatingTextarea2">Commentaire</label>
                             <textarea class="form-control" placeholder="Message" id="floatingTextarea2" style="height: 100px"></textarea>
 
                         </div>
